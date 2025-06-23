1ca449b5793b51013e4383c3546ccacce9b3f0e0
FirdausKz <espejodaniel50@gmail.com>
2025-06-23 13:38:27 +0700

    allow bot to answer any questions

--- src/telegrambotadmin/a.java
+++
@@ -1,7 +0,0 @@
-import java.sql.Connection;
-import java.sql.Statement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import javax.swing.table.DefaultTableModel;
-import javax.swing.JOptionPane;
-
